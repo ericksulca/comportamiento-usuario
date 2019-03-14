0d764a8ba2220776e5e79b16754d3230ede8fa20
Erland <blackadress.01@gmail.com>
2019-03-14 14:37:33 -0500

    websockets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import ws from 'ws'
+
+import url from 'url'
+import http from 'http'
 
 // Importando configuraciones de la BD
 import configDB from './libs/configDB'
@@ -9,7 +13,7 @@ import configDB from './libs/configDB'
 import rutaHome from './routes/home'
 import rutaCliente from './routes/cliente'
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 let app = express()
 
 app.use(bodyParser.json())
@@ -21,7 +25,7 @@ mongoose.connect(configDB.url + configDB.database, configDB.params)
     console.log('Conexion a base de datos exitosa')
   })
   .catch(err => {
-    console.error('Error en la conexion a la base de datos')
+    console.error('Error en la conexion a la base de datos', err)
   })
 
 let db = mongoose.connection
@@ -30,6 +34,39 @@ let db = mongoose.connection
 rutaHome(app)
 rutaCliente(app)
 
-app.listen(PORT, () => {
+// WEB SOCKETS SERVER
+let server = http.Server(app)
+
+let wssClient = new ws.Server({
+  server: server,
+  path: '/client'
+})
+
+function noop() {}
+function heartbeat() {
+  this.isAlive = true
+}
+const clientInterval = setInterval(function ping() {
+  wssClient.clients.forEach(ws => {
+    if (ws.isAlive === false) return ws.terminate()
+    ws.isAlive = false
+    ws.ping(noop)
+  })
+}, 30000)
+
+let userSockets = {}
+
+wssClient.on('connection', (socket, request) => {
+  socket.isAlive = true
+  socket.on('pong', heartbeat)
+
+  const parameters = url.parse(request.url, true)
+  const userId = parameters.query.user_id
+  userSockets[userId] = socket
+  console.log('Conexion establecida para usuario', userId)
+})
+
+// server listening
+server.listen(PORT, () => {
   console.log("Aplicacion corriendo en puerto:", PORT)
 })
