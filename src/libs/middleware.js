9d1384951c872f58c7b626d48d163cac6ee867b2
Erland Huamán <blackadress.01@gmail.com>
2019-03-28 14:28:39 -0500

    comentarios

--- src/libs/middleware.js
+++ src/libs/middleware.js
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 module.exports = app => {
-  let allowedOrigins = ['http://localhost:8000', 'http://192.168.2.67:8100']
+  let allowedOrigins = ['http://localhost:8000', 'http://192.168.2.67:8100'/*192.168.2.67 no funciona como allowed*/]
   let corsOptions = {
     origin: (origin, callback) => {
       if (allowedOrigins.indexOf(origin) !== -1) {
