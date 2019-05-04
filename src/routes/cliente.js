de96c3e92e1e5634e7e36935693ca4e150a68620
Erland Huamán <blackadress.01@gmail.com>
2019-05-04 09:42:24 -0500

    cliente POST

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -15,74 +15,29 @@ export default app => {
       const productosObj = req.body.productos
 
       if (clicksObj && !tagsObj && !productosObj) {
-        const { nombreObjeto, cantidad } = clicksObj[0]
-        Cliente.findOne({
-          "id": id,
-          "clicks.nombreObjeto": nombreObjeto
-        }).exec()
-          .then(result => {
-            if (result) {
-              Cliente.updateOne(
-                { "id": id , "clicks.nombreObjeto": nombreObjeto },
-                { $inc: { "clicks.$.cantidad": cantidad } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            } else {
-              Cliente.updateOne(
-                { "id": id },
-                { $push: { clicks: clicksObj } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            }
-          })
+        let cliente = new Cliente({
+          id: id,
+          clicks: clicksObj
+        })
+        cliente.save()
+          .then(result => res.json(result))
+          .catch(err => res.status(412).json(err))
       } else if (!clicksObj && tagsObj && !productosObj) {
-        const { nombreTag, puntuacion } = tagsObj[0]
-        Cliente.findOne({
-          "id": id,
-          "tags.nombreTag": nombreTag
-        }).exec()
-          .then(result => {
-            if (result) {
-              Cliente.updateOne(
-                { "id": id, "tags.nombreTag": nombreTag },
-                { $set: { "tags.$.puntuacion": puntuacion } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            } else {
-              Cliente.updateOne(
-                { "id": id },
-                { $push: { tags: tagsObj } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            }
-          })
+        let cliente = new Cliente ({
+            id: id,
+            tags: tagsObj
+        })
+        cliente.save()
+          .then(result => res.json(result))
+          .catch(err => res.status(412).json(err))
       } else if (!clicksObj && !tagsObj && productosObj) {
-        const { productoId, puntuacion } = productosObj[0]
-        Cliente.findOne({
-          "id": id,
-          "productos.productoId": productoId
-        }).exec()
-          .then(result => {
-            if (result) {
-              Cliente.updateOne(
-                {"id": id, "productos.productoId": productoId },
-                { $set: { "productos.$.puntuacion": puntuacion } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            } else {
-              Cliente.updateOne(
-                { "id": id },
-                { $push: { productos: productosObj } }
-              ).exec()
-                .then(result => res.json(result))
-                .catch(err => res.status(412).json({ msg: err.message }))
-            }
-          })
+        let cliente = new Cliente({
+          id: id,
+          productos: productosObj
+        })
+        cliente.save()
+          .then(result => res.json(result))
+          .catch(err => res.status(412).json(err))
       } else {
         let cliente = new Cliente({
           id: id,
