17e7886924eb1794464df32b95f990efac55ca62
Erland Huamán <blackadress.01@gmail.com>
2019-05-17 12:15:11 -0500

    dbg

--- src/routes/clienteWS.js
+++ src/routes/clienteWS.js
@@ -79,15 +79,15 @@ export default (app, server) => {
       const url_superusuarios = 'http://localhost:8003/superusuario/1'
       fetchSuperusuarios(url_superusuarios)
         .then(superusuarios => {
-          superusuarios = superusuarios
+          let sockets = superusuarios
             .map(superusuarioId => {
               console.log('superusuarioId', superusuarioId)
               console.log('userSockets readyState', userSockets[superusuarioId].readyState)
               console.log('ws.OPEN', ws.OPEN)
               return userSockets[superusuarioId]
             })
-          console.log('nuevo superusuarios', superusuarios)
-          superusuarios = superusuarios
+          console.log('nuevo sockets', sockets)
+          sockets = sockets
             .map(socket => {
               console.log('socket filter', socket.readyState)
               if (typeof (socket) !== 'undefined' && socket.readyState === ws.OPEN){
@@ -96,8 +96,8 @@ export default (app, server) => {
                 return
               }
             })
-          console.log('nuevo superusuarios 2', superusuarios)
-          superusuarios
+          console.log('nuevo sockets 2', sockets)
+          sockets
             .map(socketValido => {
               console.log('socket valido')
               socketValido.send(JSON.stringify(req.body))
